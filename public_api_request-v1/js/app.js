906fa3d0b95018fed5cf315b0fa5966ba6d7f6ae
jim9112 <jim9112@gmail.com>
2018-12-23 17:11:37 -0500

    added gallery view

--- public_api_request-v1/js/app.js
+++ public_api_request-v1/js/app.js
@@ -6,12 +6,17 @@ fetch('https://randomuser.me/api/?results=12')
         createGallery(userList);
     });
 
+
+    // Creates the cards of the employee dir
     const createGallery = (userList) => {
         const gallery = document.getElementById('gallery');
         
         for(let i = 0; i<userList.length; i++) {
-            var card = document.createElement('div');
+            const card = document.createElement('div');
             card.className = 'card';
+            var cardHTML = `<div class="card-img-container"><img class="card-img" src="${userList[i].picture.medium}" alt="profile picture"></div>`;
+            cardHTML += `<div class="card-info-container"><h3 id="name" class="card-name cap">${userList[i].name.first} ${userList[i].name.last}</h3><p class="card-text">${userList[i].email}</p><p class="card-text cap">${userList[i].location.city}, ${userList[i].location.state}</p></div>`;
+            card.innerHTML = cardHTML;
             gallery.append(card);
         }
     };
