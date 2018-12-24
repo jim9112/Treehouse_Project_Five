2ae124a2f0efa64a031edaaeefe49ff8d489b8ad
jim9112 <jim9112@gmail.com>
2018-12-23 21:16:46 -0500

    finished meets standards

--- public_api_request-v1/js/app.js
+++ public_api_request-v1/js/app.js
@@ -1,6 +1,6 @@
 const gallery = document.getElementById('gallery');
 
-fetch('https://randomuser.me/api/?results=12')
+fetch('https://randomuser.me/api/?results=12&nat=us')
     .then(res => res.json())
     .then(data => {
         const userList = data.results;
@@ -13,32 +13,44 @@ fetch('https://randomuser.me/api/?results=12')
     const createGallery = (userList) => {
         for(let i = 0; i<userList.length; i++) {
             const card = document.createElement('div');
-            card.className = 'card';
-            var cardHTML = `<div class="card-img-container"><img class="card-img" src="${userList[i].picture.medium}" alt="profile picture"></div>`;
-            cardHTML += `<div class="card-info-container"><h3 id="name" class="card-name cap">${userList[i].name.first} ${userList[i].name.last}</h3><p class="card-text">${userList[i].email}</p><p class="card-text cap">${userList[i].location.city}, ${userList[i].location.state}</p></div>`;
+            card.className = `card` ;
+            card.id = userList[i].name.last;
+            var cardHTML = `<div class="card-img-container"><img class="card-img" src="${userList[i].picture.large}" alt="profile picture"></div>`;
+            cardHTML += `<div class="card-info-container"><h3 id="name" class="card-name cap">${userList[i].name.first} ${userList[i].name.last}</h3><p class="card-text">${userList[i].email}</p><p class="card-text cap">${userList[i].location.city}</p></div>`;
             card.innerHTML = cardHTML;
             gallery.append(card);
         }
         modalTrigger(userList);
     };
-    const modalTrigger = (userList) => {
-        gallery.addEventListener('click', (e) => {
-            if (e.target.className === 'card' || e.target.parentNode.parentNode.className === 'card' || e.target.parentNode.className === 'card'){
-            
-                createModal(userList);
-            }
-        });
-    };
-    const createModal = (userList) => {
+
+    // create modal
+    const createModal = (userList, i) => {
         const modalContainer = document.createElement('div');
         modalContainer.className = 'modal-container';
         modalHTML = `<div class="modal"><button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>`;
-        modalHTML += `<div class="modal-info-container"></div>`;
-        
-        
-        modalHTML += `</div>`;
+        modalHTML += `<div class="modal-info-container"><img class="modal-img" src="${userList[i].picture.large}" alt="profile picture">`;
+        modalHTML += `<h3 id="name" class="modal-name cap">${userList[i].name.first} ${userList[i].name.last}</h3>`;
+        modalHTML += `<p class="modal-text">${userList[i].email}</p>`;
+        modalHTML += `<p class="modal-text cap">${userList[i].location.city}</p><hr>`;
+        modalHTML += `<p class="modal-text">${userList[i].phone}</p>`;
+        modalHTML += `<p class="modal-text cap">${userList[i].location.street}, ${userList[i].location.city}, ${userList[i].location.state} ${userList[i].location.postcode}</p>`;
+        var bDayRaw = userList[i].dob.date.slice(0,userList[i].dob.date.indexOf("T")).split("-");
+        var bDayFinal = `${bDayRaw[1]}/${bDayRaw[2]}/${bDayRaw[0]}`;
+        modalHTML += `<p class="modal-text">Birthday: ${bDayFinal}</p>`;
+        modalHTML += `</div></div>`;
         modalContainer.innerHTML = modalHTML;
         const body = document.querySelector('body');
         body.append(modalContainer);
-        document.querySelector('#modal-close-btn').addEventListener('click', ()=> body.removeChild(body.lastChild));
+        document.querySelector('#modal-close-btn').onclick = () => body.removeChild(body.lastChild);
+    };
+
+
+    const modalTrigger = (userList) => {
+        for (i = 0; i<gallery.children.length; i++) { 
+            ((i) => {
+            gallery.children[i].onclick = () => {
+                createModal(userList, i);
+            };
+        })(i);
+        }
     };
