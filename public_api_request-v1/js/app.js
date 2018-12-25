fdaa529fa7c69d44cd68d575117b08001c36f087
jim9112 <jim9112@gmail.com>
2018-12-24 19:01:41 -0500

    Added Searchbox functionality

--- public_api_request-v1/js/app.js
+++ public_api_request-v1/js/app.js
@@ -8,9 +8,29 @@ fetch('https://randomuser.me/api/?results=12&nat=us')
         const userList = data.results;
         console.log(userList);
         createGallery(userList);
+        searchBox(userList);
     });
 
-
+    const searchBox = (userList) => {
+        const searchContainer = document.querySelector('.search-container');
+        searchContainer.innerHTML = '<form action="#" method="get"><input type="search" id="search-input" class="search-input" placeholder="Search..."><input type="submit" value="&#x1F50D;" id="serach-submit" class="search-submit"></form>';
+        const searchBox = document.getElementById('search-input');
+        searchBox.onkeyup = () => {
+            for(i=0; i<gallery.children.length; i++) {
+                var name = userList[i].name.first + ' ' + userList[i].name.last;
+                
+                if (searchBox.value === '') {
+                    gallery.children[i].style.display = '';
+                }
+                else if(name.startsWith(searchBox.value.toLowerCase())){
+                    gallery.children[i].style.display = '';
+                } else {
+                    gallery.children[i].style.display = 'none';
+                }
+            }
+        };
+    };
+    
     // Creates the cards of the employee dir
     const createGallery = (userList) => {
         for(let i = 0; i<userList.length; i++) {
@@ -47,9 +67,7 @@ fetch('https://randomuser.me/api/?results=12&nat=us')
         var bDayRaw = userList[i].dob.date.slice(0,userList[i].dob.date.indexOf("T")).split("-");
         var bDayFinal = `${bDayRaw[1]}/${bDayRaw[2]}/${bDayRaw[0]}`;
         modalHTML += `<p class="modal-text">Birthday: ${bDayFinal}</p>`;
-        modalHTML += `</div><div id="cycle">`;
-        
-        
+        modalHTML += `</div><div id="cycle">`;        
         // adds previous button only when not the first record and add next button only when not the last record
         if (i>0){
             modalHTML += `<button type="button" id="modal-prev" class="modal-prev btn">Prev</button>`;
@@ -60,6 +78,7 @@ fetch('https://randomuser.me/api/?results=12&nat=us')
         modalHTML += `</div></div>`;
         modalContainer.innerHTML = modalHTML;
         body.append(modalContainer);
+        // event listener for exit button on modal
         document.querySelector('#modal-close-btn').onclick = () => deletModal();
         cycleModal(userList, i);
     };
